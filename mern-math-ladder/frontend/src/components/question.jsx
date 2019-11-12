0562bf35474b75e967bfa45d9cab4449a9ba483f
Sakshi <3135sakshi@gmail.com>
2019-11-11 18:38:15 -0700

    Question displayed on next page

--- mern-math-ladder/frontend/src/components/question.jsx
+++ mern-math-ladder/frontend/src/components/question.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import {getAssignment} from './userFunctions';
 
 class question extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+          assignment: [],
+          assignmentList: []
+        }
+        console.log(window.location.href);
+        var url = window.location.href.split('/');
+        var lastSegment = url.pop();
+        console.log(lastSegment);
+        getAssignment(lastSegment)
+        .then(res => {
+            this.setState({
+                assignment:res
+            })
+        })
+      }
+
     render(){
+        console.log(this.state.assignment.options);
         return (
             <div className="container">
-            This is your question
-        </div>
+               Question :  {this.state.assignment.question} ?
+               <ul>
+                  
+               </ul>
+            </div>
         ) 
     }
 }
