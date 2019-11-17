0d000ef0b3fdb221f804a26f022f746d5da4f9aa
Chanyadav <yadavchan23@gmail.com>
2019-11-16 20:41:06 -0700

    Changes for Student Profile

---
+++ mern-math-ladder/frontend/src/components/StudentProfile.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import {solveAssignmentCreatedByUser} from './userFunctions'
+import {addAssignment} from './userFunctions'
+import {assignmentCreatedByUser} from './userFunctions'
+import {getallAssignments} from './userFunctions'
+
+//import {answerAssignment}from './userFunctions'
+var b = [];
+
+class StudentProfile extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      assignment: [],
+      assignmentList: []
+    }
+  }
+  
+  handleSelect(id) {
+    const { history } = this.props;
+    history.push(`/getallAssignments/${id}`);
+  }
+
+  
+
+  createAssignment() {
+
+    const assignment = {
+      question : this.state.question,
+      options : this.state.options
+    }
+
+    addAssignment(assignment)
+    .then(res => {
+      if(res)
+      {
+        alert(res.status);
+      }else{
+        alert("Some Error occured while creating assignment");
+      }
+     
+      this.setState({
+        assignment: res
+      });
+    });
+  }
+
+  
+onAssignmentClick() {
+  getallAssignments()
+  .then(res => getallAssignments())
+  .then(res => {
+    this.setState({
+      assignmentList: res
+    });
+
+    for(var i=1; i<=res.length; i++){
+     b.push(i);
+    }
+  });
+}
+
+routeChange() {
+  const { history } = this.props;
+  history.push('/AddQuestion');
+}
+
+    
+    render(){
+        return (
+            
+            <div className="container">
+              <div className="buttons_center">
+                
+                <button  onClick = {this.onAssignmentClick.bind(this)}>
+                  My Assignments
+                </button>
+              </div>
+              <div>
+                <ul>
+                  {this.state.assignmentList && this.state.assignmentList.map(function(listValue, i){
+                  return <li><a href ={"#/getAssignmentForStudent/" + listValue._id}>Assignment {i+1}</a>
+                  </li>;
+                  })}         
+                </ul>
+              </div>
+            </div>
+        ); 
+    }
+
+}
+
+export default StudentProfile
---
+++ mern-math-ladder/frontend/src/components/answerQuestion.jsx
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import {solveAssignmentCreatedByUser} from './userFunctions'
+import {getAssignment} from './userFunctions'
+var choices  = [];
+
+class answerQuestion extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            question: "2 + 3",
+            option1: '',
+            option2: '',
+            option3: '',
+            option4: '',
+            assignment: [],
+            assignmentList: [],
+            options: [],
+            optionsToDisplay:["5", "4", "3", "2"],
+            answer: "5"
+        }; 
+
+        var url = window.location.href.split('/');
+        var lastSegment = url.pop();
+        getAssignment(lastSegment)
+        .then(res => {
+            console.log("Option 1 :- ",res.options[0].option);
+            this.setState({
+                assignment:res,
+                options: [this.state.option1, this.state.option2, this.state.option3, this.state.option4],
+                option1: res.options[0].option,
+                option2: res.options[1].option,
+                option3: res.options[2].option,
+                option4: res.options[3].option 
+            })
+        })
+//  console.log("choices");
+//         var choices = this.state.options;
+//         console.log(choices);
+//         this.state.option1 = choices ? choices[0].option : "Option 1";
+//         this.state.option2 = choices ? choices[1].option : "Option 2";
+//         this.state.option3 = choices ? choices[2].option : "Option 3";
+//         this.state.option4 = choices ? choices[3].option : "Option 4";
+        
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+       console.log("state" , this.state.option1);
+       var url = window.location.href.split('/');
+       var lastSegment = url.pop();
+        const assignment = {
+            question : this.state.question,
+            options :  [this.state.option1, this.state.option2, this.state.option3, this.state.option4],
+            answer: this.state.answer,
+            parameter: lastSegment
+          }
+          console.log("call to frontend api");
+          console.log(assignment);
+          solveAssignmentCreatedByUser(assignment)
+          .then(res => {
+              console.log("response")
+              console.log(res);
+            if(res)
+            {
+              alert(res.status);
+            }else{
+              alert("Some Error occured while creating assignment");
+            }
+           
+            this.setState({
+              assignment: res
+            });
+          });
+    }
+
+    render() {
+        //console.log(this.state.options)
+        return (
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 mt-5 mx-auto">
+                <form validate onSubmit={this.onSubmit}>
+                  <div className="form-group">
+        <label htmlFor="question"> Question : {this.state.assignment.question}</label>
+                    
+                  </div>
+                  <div className="form-group">
+        <label htmlFor="option1"> {this.state.option1} </label>
+                    <input
+                      type="checkbox"
+                      className="form-control"
+                      name="option1"
+                      placeholder="Option"
+                      value={this.state.option1}
+                     // onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="option2">Option 2 </label>
+                    <input
+                      type="option2"
+                      className="form-control"
+                      name="option2"
+                      placeholder="Option"
+                      value={this.state.option2}
+                    //  onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="option3">Option 3 </label>
+                    <input
+                      type="option3"
+                      className="form-control"
+                      name="option3"
+                      placeholder="Option"
+                      value={this.state.option3}
+                     // onChange={this.onChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="option1">Option 4 </label>
+                    <input
+                      type="option4"
+                      className="form-control"
+                      name="option4"
+                      placeholder="Option"
+                      value={this.state.option4}
+                    //  onChange={this.onChange}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-lg btn-primary btn-block"
+                  >
+                   Submit Answer
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        )
+      }
+}
+
+export default answerQuestion  
